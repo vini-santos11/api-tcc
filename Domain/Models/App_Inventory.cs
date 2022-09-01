151b46aa23beb1148a0470cf68f35cff5cf73cdf
Marcus Vinícius <m.vinicius1122o@gmail.com>
2022-09-01 20:37:05 -0300

    feat: :tada: criação de todos os models seguindo o banco de dados

---
+++ Domain/Models/App_Inventory.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Contrib.Extensions;
+using Domain.Models.Base;
+
+namespace Domain.Models
+{
+    [Table("db_tcc.App_Inventory")]
+    public class App_Inventory : CreatedModel
+    {
+        [Key]
+
+        public long Id { get; set; }
+        public long ProductId { get; set; }
+        public double Amount { get; set; }
+    }
+}
---
+++ Domain/Models/App_StockMovement.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Contrib.Extensions;
+using Domain.Models.Base;
+
+namespace Domain.Models
+{
+    [Table("db_tcc.App_StockMovement")]
+    public class App_StockMovement : CreatedModel
+    {
+        [Key]
+        public long Id { get; set; }
+        public long TransactionItemOriginId { get; set; }
+        public long TransactionItemDestinationId { get; set; }
+        public double Amount { get; set; }
+    }
+}
